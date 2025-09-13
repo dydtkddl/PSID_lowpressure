6b37367f59f4310056d556a557308d24add3a3c0
dydtkddl <dydtkddhkdwk@gmail.com>
2025-09-14 03:21:36 +0900

    123

--- Training_Structure/make_commands_ver13.py
+++ Training_Structure/make_commands_ver13.py
@@ -5,10 +5,11 @@ import math
 
 
 def generate_commands():
-    TEMPS = [293]#,273,  293, 313]
+    TEMPS = [273,  293, 313]
     GASES = ["He", "Ar"]
-    GASES = ["N2", "O2", "H2"]
+#    GASES = ["N2", "O2", "H2"]
     TRAINRATIOS = [0.05, 0.1 , 0.15 , 0.2 , 0.25 ,0.3 ,0.35 ,0.4 ,0.45, 0.5 ,0.55 ,0.6, 0.65, 0.7, 0.75, 0.8]
+    TRAINRATIOS = [0.8]
 
     SAMPLINGS = [
         ("qt_then_rd", "struct+input"),
@@ -16,12 +17,12 @@ def generate_commands():
         ("random_struct", "struct"),
                  ]
     # SAMPLINGS = ["random_struct"]
-    MODELS = ["cat"] # ,"rf", "gbm"]
+    MODELS = ["cat" ,"rf", "gbm"]
     SEED = 52
     TRIALS = [1, 2, 3, 4, 5]
-    LOWS = [ 0.01]#, 0.05, 0.1, 0.5]
+    LOWS = [ 0.01, 0.05, 0.1, 0.5]
     HIGHS = [1,5, 15]
-    HIGHS = [5]
+#    HIGHS = [5]
     commands = []
 
     total_combos = len(TEMPS) * len(GASES) * len(TRAINRATIOS) * len(SAMPLINGS) * len(MODELS) * len(TRIALS)
